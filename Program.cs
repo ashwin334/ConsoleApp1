0fbe7664f6df32e86a1975ba8925fce440fb0e9a
ashwin333 <ashwinpatil832@gmail.com>
2022-10-06 13:21:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 2022;
+            string month="Sept";
+            float percentage = 89.65F;
+            double a = 89.66;
+            // using + sign
+            Console.WriteLine("Current year is "+year+" & month is "+month);
+
+            //placeholder {0}
+
+            Console.WriteLine("Current year is {0} & month is {1}" ,year, month);
+
+            //interpolation--> $ sign
+
+            Console.WriteLine($" Current year is {year}  & month is {month}");
+            Console.WriteLine(year);
+            Console.WriteLine(month);
+            Console.WriteLine(percentage);
+
+        }
+    }
+}
